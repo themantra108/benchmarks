cc92f8e069f4e4db1fdab7e69bf97c0122cae5d6
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 02:44:23 +0530

    u1

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -12,18 +12,15 @@ end
 function run_hello_world_benchmark(language::String, extension::String)
     script_path = "script/$language/helloworld.$extension"
     if isfile(script_path)
+        @info "Benchmarking $language Hello World:"
         if language == "rust"
-            @info "Benchmarking $language Hello World:"
             @benchmark run(`rustc --crate-name helloworld $script_path -o script/$language/helloworld`)
             @benchmark run(`script/$language/helloworld`)
         elseif language == "julia"
-            @info "Benchmarking $language Hello World:"
             @benchmark run(`julia --project=. $script_path`)
         elseif language == "python"
-            @info "Benchmarking $language Hello World:"
             @benchmark run_python_script(script_path)
         else
-            @info "Benchmarking $language Hello World:"
             @benchmark run(`$language $script_path`)
         end
     else
