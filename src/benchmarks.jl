32d45c133156ee149d51db1eb9900dd07ab642b4
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 02:41:01 +0530

    rm cc

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -14,17 +14,17 @@ function run_hello_world_benchmark(language::String, extension::String)
     if isfile(script_path)
         if language == "rust"
             @info "Benchmarking $language Hello World:"
-            @benchmark run(`rustc --crate-name helloworld $script_path -o script/$language/helloworld`) setup=(BenchmarkTools.compilecache())
-            @benchmark run(`script/$language/helloworld`) setup=(BenchmarkTools.compilecache())
+            @benchmark run(`rustc --crate-name helloworld $script_path -o script/$language/helloworld`)
+            @benchmark run(`script/$language/helloworld`)
         elseif language == "julia"
             @info "Benchmarking $language Hello World:"
-            @benchmark run(`julia --project=. $script_path`) setup=(BenchmarkTools.compilecache())
+            @benchmark run(`julia --project=. $script_path`)
         elseif language == "python"
             @info "Benchmarking $language Hello World:"
-            @benchmark run_python_script(script_path) setup=(BenchmarkTools.compilecache())
+            @benchmark run_python_script(script_path)
         else
             @info "Benchmarking $language Hello World:"
-            @benchmark run(`$language $script_path`) setup=(BenchmarkTools.compilecache())
+            @benchmark run(`$language $script_path`)
         end
     else
         @warn "No Hello World script found for $language"
