8f0bb93b12fb36e1653206ffbcd863271772a4f1
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 02:57:00 +0530

    modi

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -1,41 +1,19 @@
-module benchmarks
-
-using BenchmarkTools
-
-# Execute a Python script from Julia
-function run_python_script(script_path::AbstractString)
-    cmd = `python $script_path`
-    run(cmd)
-end
-
-# Benchmark Hello World script for a specific language
 function run_hello_world_benchmark(language::String, extension::String)
-    script_path = joinpath("script", language, "helloworld.$extension")
+    script_dir = dirname(@__FILE__)
+    script_path = joinpath(script_dir, "script", language, "helloworld.$extension")
     if isfile(script_path)
-        @info "Benchmarking $language Hello World:"
+        @info "Running $language Hello World:"
         if language == "rust"
-            @benchmark run(`rustc --crate-name helloworld $script_path -o script/$language/helloworld`)
-            @benchmark run(`script/$language/helloworld`)
+            run(`rustc --crate-name helloworld $script_path -o $script_dir/script/$language/helloworld`)
+            run(`$script_dir/script/$language/helloworld`)
         elseif language == "julia"
-            @benchmark run(`julia --project=. $script_path`)
+            run(`julia --project=$script_dir $script_path`)
         elseif language == "python"
-            @benchmark run_python_script(script_path)
+            run(`python $script_path`)
         else
-            @benchmark run(`$language $script_path`)
+            run(`$language $script_path`)
         end
     else
         @warn "No Hello World script found for $language"
     end
 end
-
-# Benchmark all Hello World scripts
-function benchmark_all()
-    languages = ["julia", "python", "go", "rust"]
-    extensions = ["jl", "py", "go", "rs"]
-
-    for (language, extension) in zip(languages, extensions)
-        run_hello_world_benchmark(language, extension)
-    end
-end
-
-end  # module benchmarks
