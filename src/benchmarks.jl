63c49ee140f4ffa70717c1e76e5ffc929b886705
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 11:41:06 +0530

    build and run

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -1,5 +1,45 @@
 module benchmarks
 
 # Write your package code here.
+# benchmarks.jl
+
+function run_rust()
+    # Build Rust program
+    cmd = `rustc hello_world.rs`
+    run(cmd)
+    
+    # Execute Rust program
+    cmd = `./hello_world`
+    run(cmd)
+end
+
+function run_go()
+    # Build Go program
+    cmd = `go build hello_world.go`
+    run(cmd)
+    
+    # Execute Go program
+    cmd = `./hello_world`
+    run(cmd)
+end
+
+function run_python()
+    # Execute Python program
+    cmd = `python hello_world.py`
+    run(cmd)
+end
+
+function run_julia()
+    # Execute Julia program
+    cmd = `julia hello_world.jl`
+    run(cmd)
+end
+
+# Run benchmarks
+@time run_rust()
+@time run_go()
+@time run_python()
+@time run_julia()
+
 
 end
