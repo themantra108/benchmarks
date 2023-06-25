5254c8d9fa49ce4d18fca234779f8ced6c01b0c9
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 22:44:14 +0530

    a

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -4,56 +4,60 @@ const PKGDIR = pkgdir(benchmarks)
 
 export run_command
 
-function run_rust()
-    # Specify the path to hello_world.rs
-    rust_file = joinpath(PKGDIR,"script", "rust", "hello_world.rs")
+function run_rust(filename::String)
+    # Specify the path to the Rust file
+    rust_file = joinpath(PKGDIR, "script", "rust", "$filename.rs")
 
     # Build Rust program
     cmd = `rustc $rust_file`
     run(cmd)
     
     # Execute Rust program
-    cmd = `./hello_world`
+    cmd = `./$filename`
     run(cmd)
 end
 
-function run_go()
-    # Specify the path to hello_world.go
-    go_file = joinpath(PKGDIR,"script", "go", "hello_world.go")
+function run_go(filename::String)
+    # Specify the path to the Go file
+    go_file = joinpath(PKGDIR, "script", "go", "$filename.go")
     
     # Build Go program
     cmd = `go build $go_file`
     run(cmd)
     
     # Execute Go program
-    cmd = `./hello_world`
+    cmd = `./$filename`
     run(cmd)
 end
 
-function run_python()
-    # Specify the path to hello_world.py
-    python_file = joinpath(PKGDIR,"script", "python", "hello_world.py")
+function run_python(filename::String)
+    # Specify the path to the Python file
+    python_file = joinpath(PKGDIR, "script", "python", "$filename.py")
     
     # Execute Python program
     cmd = `python3 $python_file`
     run(cmd)
 end
 
-function run_julia()
-    # Specify the path to hello_world.jl
-    julia_file = joinpath(PKGDIR,"script", "julia", "hello_world.jl")
+function run_julia(filename::String)
+    # Specify the path to the Julia file
+    julia_file = joinpath(PKGDIR, "script", "julia", "$filename.jl")
     
     # Execute Julia program
     cmd = `julia $julia_file`
     run(cmd)
 end
 
+files = ["hello_world"]
+
 function run_command()
     # Run benchmarks
-    @time run_rust()
-    @time run_go()
-    @time run_python()
-    @time run_julia()
+    for file in files
+        @time run_rust(file)
+        @time run_go(file)
+        @time run_python(file)
+        @time run_julia(file)
+    end
 end
 
 end  # module benchmarks
