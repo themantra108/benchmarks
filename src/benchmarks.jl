62bee44dbe8b2605e00d03ba0724fa5a1691c189
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 13:17:58 +0530

    u2

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -4,7 +4,7 @@ export run_command
 
 function run_rust()
     # Specify the path to hello_world.rs
-    rust_file = joinpath(@__DIR__, "hello_world.rs")
+    rust_file = joinpath(@__DIR__, "rust", "hello_world.rs")
 
     # Build Rust program
     cmd = `rustc $rust_file`
@@ -16,8 +16,11 @@ function run_rust()
 end
 
 function run_go()
+    # Specify the path to hello_world.go
+    go_file = joinpath(@__DIR__, "go", "hello_world.go")
+    
     # Build Go program
-    cmd = `go build hello_world.go`
+    cmd = `go build $go_file`
     run(cmd)
     
     # Execute Go program
@@ -26,26 +29,29 @@ function run_go()
 end
 
 function run_python()
-    python_file = joinpath(@__DIR__, "hello_world.py")
+    # Specify the path to hello_world.py
+    python_file = joinpath(@__DIR__, "python", "hello_world.py")
+    
     # Execute Python program
     cmd = `python $python_file`
     run(cmd)
 end
 
 function run_julia()
-    julia_file = joinpath(@__DIR__, "hello_world.jl")
+    # Specify the path to hello_world.jl
+    julia_file = joinpath(@__DIR__, "julia", "hello_world.jl")
+    
     # Execute Julia program
     cmd = `julia $julia_file`
     run(cmd)
 end
 
 function run_command()
-    @time run_julia()
     # Run benchmarks
     @time run_rust()
-    # @time run_go()
+    @time run_go()
     @time run_python()
-    
+    @time run_julia()
 end
 
 end  # module benchmarks
