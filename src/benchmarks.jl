781d4b231e4d026913b962e92d3e8e8f4aa29163
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 13:54:06 +0530

    patch++

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -2,53 +2,45 @@ module benchmarks
 
 export run_command
 
-function build_languages(languages::Vector{String})
-    for language in languages
-        folder = joinpath(@__DIR__, language)
-        for file in readdir(folder)
-            path = joinpath(folder, file)
-            if !isdir(path)
-                if language == "rust"
-                    cmd = `rustc $path`
-                    run(cmd)
-                elseif language == "go"
-                    cmd = `go build $path`
-                    run(cmd)
-                end
-            end
-        end
+function build_program(file::AbstractString, lang::Symbol)
+    if lang == :rust
+        cmd = `rustc $file`
+    elseif lang == :go
+        cmd = `go build $file`
+    else
+        error("Unsupported language: $lang")
     end
+    run(cmd)
 end
 
-function run_languages(languages::Vector{String})
-    for language in languages
-        folder = joinpath(@__DIR__, language)
-        for file in readdir(folder)
-            path = joinpath(folder, file)
-            if !isdir(path)
-                if language == "rust"
-                    cmd = `./$(basename(file, ".rs"))`
-                    run(cmd)
-                elseif language == "go"
-                    cmd = `./$(basename(file, ".go"))`
-                    run(cmd)
-                elseif language == "python"
-                    cmd = `python $path`
-                    run(cmd)
-                elseif language == "julia"
-                    cmd = `julia $path`
-                    run(cmd)
-                end
-            end
-        end
+function execute_program(file::AbstractString, lang::Symbol)
+    if lang == :rust || lang == :go
+        cmd = `./$file`
+    elseif lang == :python
+        cmd = `python $file`
+    elseif lang == :julia
+        cmd = `julia $file`
+    else
+        error("Unsupported language: $lang")
     end
+    run(cmd)
+end
+
+function run_language(lang::Symbol, file::AbstractString)
+    build_program(file, lang)
+    execute_program(file, lang)
 end
 
 function run_command()
-    languages = ["rust", "go", "python", "julia"]
-
-    @time build_languages(languages)
-    @time run_languages(languages)
+    language_folders = ["rust", "go", "python", "julia"]
+    
+    for lang in language_folders
+        files = readdir(joinpath(@__DIR__, lang))
+        for file in files
+            file_path = joinpath(@__DIR__, lang, file)
+            @time run_language(Symbol(lang), file_path)
+        end
+    end
 end
 
 end  # module benchmarks
