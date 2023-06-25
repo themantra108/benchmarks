d871a3abd8653b80d14a379fe073349abf1ebe8d
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 18:32:01 +0530

    up

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -1,10 +1,12 @@
 module benchmarks
 
+const PKGDIR = pkgdir(benchmarks)
+
 export run_command
 
 function run_rust()
     # Specify the path to hello_world.rs
-    rust_file = joinpath(@__DIR__, "rust", "hello_world.rs")
+    rust_file = joinpath(PKGDIR,"script", "rust", "hello_world.rs")
 
     # Build Rust program
     cmd = `rustc $rust_file`
@@ -17,7 +19,7 @@ end
 
 function run_go()
     # Specify the path to hello_world.go
-    go_file = joinpath(@__DIR__, "go", "hello_world.go")
+    go_file = joinpath(PKGDIR,"script", "go", "hello_world.go")
     
     # Build Go program
     cmd = `go build $go_file`
@@ -30,16 +32,16 @@ end
 
 function run_python()
     # Specify the path to hello_world.py
-    python_file = joinpath(@__DIR__, "python", "hello_world.py")
+    python_file = joinpath(PKGDIR,"script", "python", "hello_world.py")
     
     # Execute Python program
-    cmd = `python $python_file`
+    cmd = `python3 $python_file`
     run(cmd)
 end
 
 function run_julia()
     # Specify the path to hello_world.jl
-    julia_file = joinpath(@__DIR__, "julia", "hello_world.jl")
+    julia_file = joinpath(PKGDIR,"script", "julia", "hello_world.jl")
     
     # Execute Julia program
     cmd = `julia $julia_file`
