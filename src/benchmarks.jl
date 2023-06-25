54abe261e52e35a45fe2f961849fb76398777641
themantra108 <66516603+themantra108@users.noreply.github.com>
2023-06-25 12:28:13 +0530

    const pkgdir

--- src/benchmarks.jl
+++ src/benchmarks.jl
@@ -2,36 +2,42 @@ module benchmarks
 
 export run_command
 
-function run_rust()
+const PKGDIR = pkgdir("benchmarks")
+
+function build_rust()
     # Specify the path to hello_world.rs
-    rust_file = joinpath(@__DIR__, "hello_world.rs")
+    rust_file = joinpath(PKGDIR, "hello_world.rs")
 
     # Build Rust program
     cmd = `rustc $rust_file`
     run(cmd)
-    
-    # Execute Rust program
-    cmd = `./hello_world`
-    run(joinpath(@__DIR__, cmd))
 end
 
-function run_go()
+function run_rust()
+    # Execute Rust program
+    cmd = `./hello_world`
+    run(joinpath(PKGDIR, cmd))
+end
+
+function build_go()
     # Specify the path to hello_world.go
-    go_file = joinpath(@__DIR__, "hello_world.go")
-    
+    go_file = joinpath(PKGDIR, "hello_world.go")
+
     # Build Go program
     cmd = `go build $go_file`
     run(cmd)
-    
+end
+
+function run_go()
     # Execute Go program
     cmd = `./hello_world`
-    run(joinpath(@__DIR__, cmd))
+    run(joinpath(PKGDIR, cmd))
 end
 
 function run_python()
     # Specify the path to hello_world.py
-    python_file = joinpath(@__DIR__, "hello_world.py")
-    
+    python_file = joinpath(PKGDIR, "hello_world.py")
+
     # Execute Python program
     cmd = `python $python_file`
     run(cmd)
@@ -39,17 +45,21 @@ end
 
 function run_julia()
     # Specify the path to hello_world.jl
-    julia_file = joinpath(@__DIR__, "hello_world.jl")
-    
+    julia_file = joinpath(PKGDIR, "hello_world.jl")
+
     # Execute Julia program
     cmd = `julia $julia_file`
     run(cmd)
 end
 
 function run_command()
-    # Run benchmarks
+    # Build and run benchmarks
+    @time build_rust()
     @time run_rust()
+    
+    @time build_go()
     @time run_go()
+    
     @time run_python()
     @time run_julia()
 end
